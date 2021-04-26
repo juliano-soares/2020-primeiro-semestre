5f7e0daa1430068433cb285c4dbfb71016dcdeb6
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2021-04-25 21:47:42 -0300

    Add files via upload

---
+++ Inteligencia_Artificial/IA - A*/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "./Map/Map.h"
+
+using namespace std;
+
+int main() {
+
+  // Cria o mapa com 3 dimensões
+  std::cout << "Iniciando" << std::endl;
+  Map map; // x-largura; y-altura; z-comprimento;
+  return 0;
+}
